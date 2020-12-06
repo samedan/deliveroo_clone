59fb724682197acdcdc063acd3864df42c547cad
samedan <same.dan@gmail.com>
2020-12-06 19:34:40 +0100

    sunday evening

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '7f3682688403c663484ce2f906ee22d2'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '4c763cc6-2f6c-4d10-acb3-d6b3b5c61a26'
+};
