d1826b6ad47c7ff4941c156ebfe7c878f194c312
Daniel Popescu <same.dan@gmail.com>
2020-12-26 20:26:37 +0100

    Before Deploy

--- frontend/pages/restaurants.js
+++ frontend/pages/restaurants.js
@@ -1,6 +1,11 @@
+import { useContext } from "react";
 import { useQuery } from "@apollo/react-hooks";
 import { useRouter } from "next/router";
 import { gql } from "apollo-boost";
+
+
+import Cart from "../components/cart/";
+import AppContext from "../context/AppContext";
 
 import {
   Button,
@@ -31,7 +36,8 @@ const GET_RESTAURANT_DISHES = gql`
   }
 `;
 
-function Restaurants(props) {
+function Restaurants() {
+  const appContext = useContext(AppContext);
   const router = useRouter();
   const { loading, error, data } = useQuery(GET_RESTAURANT_DISHES, {
     variables: { id: router.query.id },
@@ -41,56 +47,60 @@ function Restaurants(props) {
   if (loading) return <h1>Loading ...</h1>;
   if (data.restaurant) {
     const { restaurant } = data;
-    console.log(restaurant);
     return (
       <>
         <h1>{restaurant.name}</h1>
         <Row>
-          {restaurant.dishes.map((res) => {
-            console.log(res);
-            return (
-              <Col xs="6" sm="4" style={{ padding: 0 }} key={res.id}>
-                <Card style={{ margin: "0 10px" }}>
-                  <CardImg
-                    top={true}
-                    style={{ height: 250 }}
-                    //   src={`${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`}
-                    src={`${process.env.NEXT_PUBLIC_API_URL}${res.image[0].url}`}
-                  />
-                  <CardBody>
-                    <CardTitle>{res.name}</CardTitle>
-                    <CardText>{res.description}</CardText>
-                  </CardBody>
-                  <div className="card-footer">
-                    <Button outline color="primary">
-                      + Add To Cart
-                    </Button>
+          {restaurant.dishes.map((res) => (
+            <Col xs="6" sm="4" style={{ padding: 0 }} key={res.id}>
+              <Card style={{ margin: "0 10px" }}>
+                <CardImg
+                  top={true}
+                  style={{ height: 250 }}
+                  src={`${process.env.NEXT_PUBLIC_API_URL}${res.image.url}`}
+                />
+                <CardBody>
+                  <CardTitle>{res.name}</CardTitle>
+                  <CardText>{res.description}</CardText>
+                </CardBody>
+                <div className="card-footer">
+                  <Button
+                    outline
+                    color="primary"
+                    onClick={() => appContext.addItem(res)}
+                  >
+                    + Add To Cart
+                  </Button>
 
-                    <style jsx>
-                      {`
-                        a {
-                          color: white;
-                        }
-                        a:link {
-                          text-decoration: none;
-                          color: white;
-                        }
-                        .container-fluid {
-                          margin-bottom: 30px;
-                        }
-                        .btn-outline-primary {
-                          color: #007bff !important;
-                        }
-                        a:hover {
-                          color: white !important;
-                        }
-                      `}
-                    </style>
-                  </div>
-                </Card>
-              </Col>
-            );
-          })}
+                  <style jsx>
+                    {`
+                      a {
+                        color: white;
+                      }
+                      a:link {
+                        text-decoration: none;
+                        color: white;
+                      }
+                      .container-fluid {
+                        margin-bottom: 30px;
+                      }
+                      .btn-outline-primary {
+                        color: #007bff !important;
+                      }
+                      a:hover {
+                        color: white !important;
+                      }
+                    `}
+                  </style>
+                </div>
+              </Card>
+            </Col>
+          ))}
+          <Col xs="3" style={{ padding: 0 }}>
+            <div>
+              <Cart />
+            </div>
+          </Col>
         </Row>
       </>
     );
